cadc6a0634f00b5ee009fd2fdcd1c2d93c80aa44
Trent Loyd <trent.loyd@gmail.com>
2022-04-16 20:23:20 -0400

    finished final step of challenge

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { isEmpty } = require("lodash");
+const _ = require("lodash");
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,6 +13,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 let postLog = [];
 
 const app = express();
+app.locals._ = _;
 
 app.set('view engine', 'ejs');
 
@@ -56,13 +59,17 @@ app.post("/compose", function(req, res){
 //Capture the route for /posts and print out the postname used in URL
 app.get('/posts/:postName', (req, res) => {
   //console.log(req.params.postName);
-  const titleRequest = req.params.postName;
+  const titleRequest = _.lowerCase(req.params.postName);
 
   postLog.forEach(function(post){
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title);
 
+    
     if(storedTitle === titleRequest){
-      console.log("Match Found!");
+      res.render("post",  {
+        title: post.title,
+        content: post.body
+      });
     };
   });
 });
